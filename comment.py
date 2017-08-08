7bb0ce2b48c0fb597eab268700b88504476c77de
Tiya Chowdhury <tiya@google.com>
2017-08-08 18:16:34 +0100

    Updated script to handle titles with values on next line

--- comment.py
+++ comment.py
@@ -8,14 +8,18 @@ print("\nGITLAB COMMENT\n")
 for line in f:
 	if ":" in line :
 		test = line.split(":")
-		if test[1].strip() :
-			print("**"+test[0]+"**: "+test[1].strip() + "<br />")
+		if test[1].strip() : #check for empty string
+			if "Expected" in line or "Actual" in line: 
+				print("\n**"+test[0]+"**: "+test[1].strip())
+			else :	
+				print("**"+test[0]+"**: "+test[1].strip() + "<br />")
+		elif "Test Steps" in test[0].strip() or "Pre-requisites" in test[0].strip():
+			print ("**" + test[0].strip()+ "**")
 	elif "." in line :
 		test = line.split(".")
 		print(test[0]+". "+test[1].strip()+ "<br />")
 	else :
 		print("**"+line.strip()+"**")
-
 
 ##For jira
 print("\n\nJIRA COMMENT\n")
@@ -24,10 +28,12 @@ for line in f:
 	if ":" in line :
 		test = line.split(":")
 		if test[1].strip() : #check for empty string
-			if "Expected" in line :
+			if "Expected" in line or "Actual" in line:
 				print("\n*"+test[0]+"*: "+test[1].strip())
 			else :
 				print("*"+test[0]+"*: "+test[1].strip())
+		elif "Test Steps" in test[0].strip() or "Pre-requisites" in test[0].strip():
+			print ("**" + test[0].strip()+ "**")		
 	elif "." in line :
 		test = line.split(".")
 		print("# "+test[1].strip())
