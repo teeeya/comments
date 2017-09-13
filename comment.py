74bc1aca3168559ff02fa67eb38e84154b7e39fe
Tiya Chowdhury <tiya@google.com>
2017-09-13 11:09:45 +0100

    Updated script to handle even more syntax

--- comment.py
+++ comment.py
@@ -1,45 +1,66 @@
-import os, sys
+import os, sys, re
 f=open(os.path.join(os.path.dirname(__file__), "comment.txt"))
 
 
 
 ## For gitlab
+first = True
 print("\nGITLAB COMMENT\n")
 for line in f:
-	if ":" in line :
+	if re.match("^[A-Z]", line) and (":" in line):
 		test = line.split(":")
-		if test[1].strip() : #check for empty string
-			if "Expected" in line or "Actual" in line: 
-				print("\n**"+test[0]+"**: "+test[1].strip())
-			else :	
+		if test[1].strip() :
+			if "Expected" in test[0] :
+				print("\n"+"**"+test[0]+"**: "+test[1].strip() + "<br />")
+			else: 
 				print("**"+test[0]+"**: "+test[1].strip() + "<br />")
-		elif "Test Steps" in test[0].strip() or "Pre-requisites" in test[0].strip() or "Expected" in line or "Actual" in line:
-			print ("\n**" + test[0].strip()+ "**")
-	elif "." in line :
+		else : 
+			if "Test Steps" in test[0] :
+				print("\n"+"**"+test[0]+"**: "+test[1].strip() + "<br />")
+			else:
+				print ("i am here" + line)
+				print("**"+test[0]+"**: "+ "<br />")
+	elif re.match("(^[0-9]\.)", line) :
 		test = line.split(".")
 		print(test[0]+". "+test[1].strip()+ "<br />")
 	elif "-" in line :
-		print("-"+line.strip()+ "<br />")
+		if(first):
+			test = line.split("-")
+			print("\n" + "* "+test[1].strip())
+			first = False
+		else:
+			test = line.split("-")
+			print("* "+test[1].strip())
 	else :
-		print("**"+line.strip()+"**")
+		print("**"+line.strip()+"**"+"<br />")
+
 
 ##For jira
 print("\n\nJIRA COMMENT\n")
 f.seek(0)
+
+first = True
 for line in f:
-	if ":" in line :
+	if re.match("^[A-Z]", line) and (":" in line):
 		test = line.split(":")
 		if test[1].strip() : #check for empty string
-			if "Expected" in line or "Actual" in line:
+			if "Expected" in line :
 				print("\n*"+test[0]+"*: "+test[1].strip())
 			else :
 				print("*"+test[0]+"*: "+test[1].strip())
-		elif "Test Steps" in test[0].strip() or "Pre-requisites" in test[0].strip() or "Expected" in line or "Actual" in line:
-			print ("\n*"+ test[0].strip()+ "*")		
-	elif "." in line :
+		else:
+			if "Test Steps" in test[0] :
+				print("\n"+"*"+test[0]+"*: "+test[1].strip())
+	elif re.match("(^[0-9]\.)",line) :
 		test = line.split(".")
 		print("# "+test[1].strip())
 	elif "-" in line :
-		print("-"+line.strip())	
+		if(first) :
+			test = line.split("-")
+			print("\n"+"* "+test[1].strip())
+			first=False
+		else :
+			test = line.split("-")
+			print("* "+test[1].strip())
 	else :
 		print("*"+line.strip()+"*")
